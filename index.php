065790644732c179e51f0c6f301450f81c7706df
Jual Rana <contactjual@gmail.com>
2024-12-25 12:34:55 +0600

    constant done

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
 
+// constants
+
+define ("NAME", "Jual");
+define ("PAYMENT", 2003);
+
+
+// output 
+echo NAME;
+echo "<br>"; // or ."<br>"
+echo PAYMENT; // or 
+echo constant("PAYMENT"); // 2003
+
+
+echo "My name is NAME"; // NAME constant will not work use '.' mathod. This system use for only variables.
+echo "My name is: ".NAME; // Jual
+
+
+
+
+// $("constant")------------------------------------------------
+
+define ("MY_NAME", "Jual");
+$naming = "constant";
+
+echo "My name is: " .$naming("MY_NAME"); //Jual
+
 ?>
